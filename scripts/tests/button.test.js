a9c423da7d4382d84e3e159386ac81105b4a5f00
zemaciel <>
2023-04-09 15:57:15 +0000

    Complete codealong

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,7 +5,11 @@
  const buttonClick = require("../button");
 
  beforeAll(() => {
-     document.body.innerHTML = "<p id='par'></p>";
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
  });
  
  describe("DOM tests", () => {
@@ -13,5 +17,8 @@
          buttonClick();
          expect(document.getElementById("par")
              .innerHTML).toEqual("You Clicked");
+     });
+     test("h1 should exist", () => {
+         expect(document.getElementsByTagName("h1").length).toBe(1);
      });
  });
